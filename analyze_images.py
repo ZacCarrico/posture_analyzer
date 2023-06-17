e4a8649fd7cb1bf921053d53feee68b323ae5a22
ZacCarrico <z.carrico@gmail.com>
2023-06-17 15:35:03 -0500

    make analyze_images dir option run indefinitely

--- analyze_images.py
+++ analyze_images.py
@@ -1,17 +1,24 @@
 import argparse
 import os
+import time
 from main import PoseAnalyzer
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='analyze images')
+    parser = argparse.ArgumentParser(description="analyze images")
     parser.add_argument("--path", type=str, help="path to image or dir")
     args = parser.parse_args()
 
     if os.path.isdir(args.path):
-        files = [os.path.join(args.path, f) for f in os.listdir(args.path) if os.path.isfile(os.path.join(args.path, f))]
-        for file in files:
-            PoseAnalyzer(file).save_analyzed_image()
+        while True:
+            files = [
+                os.path.join(args.path, f)
+                for f in os.listdir(args.path)
+                if os.path.isfile(os.path.join(args.path, f))
+            ]
+            for file in files:
+                PoseAnalyzer(file).save_analyzed_image()
+            time.sleep(0.1)
 
     else:
         PoseAnalyzer(args.path).save_analyzed_image()
