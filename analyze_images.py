c57202dfd338cac7533d5f34faa56847900f9c90
ZacCarrico <z.carrico@gmail.com>
2024-03-20 08:00:46 -0500

    simplify entrypoint

--- analyze_images.py
+++ analyze_images.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import gc
 import time
@@ -6,17 +5,15 @@ from main import PoseAnalyzer
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="analyze images")
-    parser.add_argument("--path", type=str, help="path to image or dir")
-    args = parser.parse_args()
+    dir = "non_analyzed_imgs"
 
-    if os.path.isdir(args.path):
+    if os.path.isdir(dir):
         print("keeps running because it's assuming pictures are constantly being added")
         while True:
             files = [
-                os.path.join(args.path, f)
-                for f in os.listdir(args.path)
-                if os.path.isfile(os.path.join(args.path, f))
+                os.path.join(dir, f)
+                for f in os.listdir(dir)
+                if os.path.isfile(os.path.join(dir, f))
             ]
             for file in files:
                 pa = PoseAnalyzer(file)
@@ -26,4 +23,4 @@ if __name__ == "__main__":
             time.sleep(0.1)
 
     else:
-        PoseAnalyzer(args.path).save_analyzed_image()
+        PoseAnalyzer(dir).save_analyzed_image()
